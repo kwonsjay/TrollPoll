15b371d10d42146ffd6dfb8fe0eb342971bf6247
SoonJae Kwon <kwonsjay@gmail.com>
2013-11-05 16:53:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrollPoll::Application.config.secret_key_base = '23f5b1abd0b166f86d6b582442ada74afd162333d4bda7fc4d3f9749be38685a95cb4782d045082e554e4b0d723054f7d60cc7de6bc2e97c9ce80a8e13566aa5'
